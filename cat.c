a1a2c0cef40326fc8b56ebe0dd69db922b5d7a84
Ryan Chang <ryanchangx@gmail.com>
2021-10-15 21:19:30 -0700

    fix exit() changes

--- cat.c
+++ cat.c
@@ -12,12 +12,12 @@ cat(int fd)
   while((n = read(fd, buf, sizeof(buf))) > 0) {
     if (write(1, buf, n) != n) {
       printf(1, "cat: write error\n");
-      
+      exit();
     }
   }
   if(n < 0){
     printf(1, "cat: read error\n");
-    
+    exit();
   }
 }
 
@@ -28,13 +28,13 @@ main(int argc, char *argv[])
 
   if(argc <= 1){
     cat(0);
-    
+    exit();
   }
 
   for(i = 1; i < argc; i++){
     if((fd = open(argv[i], 0)) < 0){
       printf(1, "cat: cannot open %s\n", argv[i]);
-      
+      exit();
     }
     cat(fd);
     close(fd);
--- init.c
+++ init.c
@@ -24,12 +24,12 @@ main(void)
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
-      
+      exit();
     }
     if(pid == 0){
       exec("sh", argv);
       printf(1, "init: exec sh failed\n");
-      
+      exit();
     }
     while((wpid=wait(0)) >= 0 && wpid != pid)
       printf(1, "zombie!\n");
